64f0768d5c923dfd4df3252950b54e8a42aff354
SruthiMalla <52434141+SruthiMalla@users.noreply.github.com>
2022-10-20 16:47:40 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status = "";
+objects = [];
 
 function preload()
 {
@@ -30,4 +31,32 @@ function modelLoaded()
 function draw()
 {
     image(video, 0, 0, 480, 380);
+
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResults);
+
+        for(i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects Detected Are: " + objects.length;
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+
+    console.log(results);
+    objects = results;
 }
